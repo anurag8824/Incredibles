0aa3eca077a5bba1a997dedef6e7b14fb27f2dbe
Infayou Digital <159997668+anurag8824@users.noreply.github.com>
2024-09-29 16:44:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json({limit:"16kb"}))
 app.use(cookieParser())
 app.use(express.static('public'));
 app.use(cors({
-    origin:["https://test-ecom-ten.vercel.app", "http://localhost:3000","http://88.222.241.94:3000" ,"http://localhost:3001","http://incredibledeals.in","http://admin.incredibledeals.in"], 
+    origin:["https://test-ecom-ten.vercel.app", "http://localhost:3000","http://88.222.241.94:3000" ,"http://localhost:3001","https://incredibledeals.in","https://admin.incredibledeals.in"], 
     credentials:true
 }));
 
