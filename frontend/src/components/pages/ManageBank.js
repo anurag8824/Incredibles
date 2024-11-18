faa2243e64642d764e127ab32fd3d7a1c74e2654
Infayou Digital <159997668+anurag8824@users.noreply.github.com>
2024-11-18 13:27:46 +0530

    Update ManageBank.js

--- frontend/src/components/pages/ManageBank.js
+++ frontend/src/components/pages/ManageBank.js
@@ -105,13 +105,7 @@ const ManageBank = () => {
 
 
 
-                <div class="flex items-start mb-6">
-                    <div class="flex items-center h-5">
-                        <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300   " required />
-                    </div>
-                    <label for="remember" class="ms-2 text-sm font-medium text-gray-900 ">I agree with the <a href="#" class="text-blue-600 hover:underline ">terms and conditions</a>.</label>
-                </div>
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
+               
             </form>
 
         </div>
